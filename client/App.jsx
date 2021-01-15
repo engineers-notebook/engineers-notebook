9287ed5a34508e7544d3005d8327ef3694c9bea9
Tommy Liang <tommyliangsays@gmail.com>
2021-01-15 15:37:24 -0500

    Setup webpack, react and express file structure

---
+++ client/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Coming from react</h1>
+    </div>
+  );
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { O_EXCL } = require('constants');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          }
+        ]
+      },
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'client/index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: { 
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        secure: false,
+        changeOrigin: true
+      }
+    },
+    publicPath: '/',
+  }
+};
+
